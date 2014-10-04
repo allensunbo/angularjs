14cc79d4d8a63b54027bda14e700528da6e2cdae
bo.sun <allensunbo@gmail.com>
2014-10-04 07:17:03 -0400

    add bootstrap

--- jwt/public/scripts/api.js
+++ jwt/public/scripts/api.js
@@ -1,11 +1,19 @@
 var app = angular.module('jwtApp', []);
 
-app.controller('MainCtrl', ['$scope', 'UserFactory', function($scope, UserFactory) {
+app.controller('MainCtrl', ['$scope', 'UserFactory', '$q', function($scope, UserFactory, $q) {
 	$scope.message = 'hello, world!';
-	UserFactory.getUser()
-	.success(function(data){
-		$scope.user = data;
-	});
+	var allUsers = [];
+	for(var i=0;i<12;i++){
+		allUsers.push(UserFactory.getUser());
+	}
+	$q.all(allUsers).then(function(results){
+		$scope.users = [];
+		for(var i=0;i<results.length;i++){
+			$scope.users.push(results[i].data);
+		}
+	}); 
+	
+	
 }]); 
 
 app.factory('UserFactory', function($http){
