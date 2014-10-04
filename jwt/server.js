7ebe0e7286cfd8d4f4284d8118c5d4017feb61c5
bo.sun <allensunbo@gmail.com>
2014-10-04 19:51:35 -0400

    test post

--- jwt/server.js
+++ jwt/server.js
@@ -4,11 +4,16 @@ var cors = require('cors');
 var jwt = require('jwt-simple');
 var moment = require('moment');
 
+var bodyParser = require('body-parser');
+
 var app = express();
 app.use(cors());
 
 var YOUR_SECRET_STRING = 'YOUR_SECRET_STRING';
 app.set('jwtTokenSecret', YOUR_SECRET_STRING);
+
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use( bodyParser.urlencoded() ); // to support URL-encoded bodies
 
 //console.log(faker);
 app.get('/random-user', function(req, res) {
@@ -31,6 +36,18 @@ app.get('/token', function(req, res) {
 		expires: expires,
 		user: user
 	});
+});
+
+app.post('/something', function(req, res, next){
+  // do something
+  var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers['x-access-token'];
+  if(token){
+    console.log(token);
+	// next();
+	res.end(token);
+  } else {
+	next();
+  }
 });
 
 app.listen(3000, function() {
